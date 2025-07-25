a049241d4d1860770da5ad2c284515dfd59dfc81
Mike <mclancy96@gmail.com>
2025-07-25 11:39:42 -0400

    Create and Read methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,29 @@
 class Movie < ActiveRecord::Base
+  def self.create_with_title(title)
+    create(title: title)
+  end
 
+  def self.first_movie
+    Movie.first
+  end
+
+  def self.last_movie
+    Movie.last
+  end
+
+  def self.movie_count
+    Movie.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+
+  def self.find_movie_with_attributes(**attrs)
+    Movie.find_by(attrs)
+  end
+
+  def self.find_movies_after_2002
+    Movie.where('release_date > ?', 2002)
+  end
 end
